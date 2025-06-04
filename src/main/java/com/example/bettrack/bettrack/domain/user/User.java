b0498f6086038f3dc33af5877a6706f9851f5ca8
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-06-04 16:12:29 -0300

    FEAT: implementando constructor

--- src/main/java/com/example/bettrack/bettrack/domain/user/User.java
+++ src/main/java/com/example/bettrack/bettrack/domain/user/User.java
@@ -1,5 +1,6 @@
 package com.example.bettrack.bettrack.domain.user;
 
+import com.example.bettrack.bettrack.dtos.UserDTO;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -19,4 +20,13 @@ public class User {
     private String document;
     @Column (unique = true)
     private String email;
+    private String password;
+
+    public User(UserDTO data){
+        System.out.println("Documento recebido" + data.document());
+        this.username = data.username();
+        this.document = data.document();
+        this.email = data.email();
+        this.password = data.password();
+    }
 }

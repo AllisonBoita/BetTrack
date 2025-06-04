4eb3f21a342cc5a66b6364d63d779f6259791825
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-06-04 16:12:06 -0300

    FEAT: adicionando campo de password

--- src/main/java/com/example/bettrack/bettrack/dtos/UserDTO.java
+++ src/main/java/com/example/bettrack/bettrack/dtos/UserDTO.java
@@ -1,4 +1,4 @@
 package com.example.bettrack.bettrack.dtos;
 
-public record UserDTO(String username, String document, String email) {
+public record UserDTO(String username, String document, String email, String password) {
 }
